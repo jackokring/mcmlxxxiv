1097e67e059b413f10ee51949dfae80beb6b279f
jackokring <jackokring@gmail.com>
2023-12-21 17:29:32 +0000

    And an icon.png needed.

---
+++ src/main/java/uk/co/kring/mcmlxxxiv/MCMLXXXIV.java
@@ -0,0 +1,12 @@
+package uk.co.kring.mcmlxxxiv;
+import net.fabricmc.api.*;
+
+public class MCMLXXXIV implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/uk/co/kring/mcmlxxxiv/client/MCMLXXXIVClient.java
@@ -0,0 +1,12 @@
+package uk.co.kring.mcmlxxxiv.client;
+import net.fabricmc.api.*;
+
+public class MCMLXXXIVClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
